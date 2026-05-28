5e771406f944567b935e760161c8138544c2178d
Geoffrey <geoffreyraditya12@gmail.com>
2026-05-28 08:42:18 +0700

    map level2

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Wajib untuk pindah level
+
+public class SceneLoader : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Ganti "Level2" dengan nama Scene map laboratorium kamu yang sebenarnya
+        SceneManager.LoadScene("Level2"); 
+    }
+}
