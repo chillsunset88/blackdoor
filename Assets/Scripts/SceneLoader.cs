ca883fcb9594d29990eb1ec4e2ed989957e7e0be
Krispyyy7 <razhib.17@gmail.com>
2026-05-28 11:33:07 +0700

    sceneloadlvl2

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,6 +6,6 @@ public class SceneLoader : MonoBehaviour
     public void StartGame()
     {
         // Ganti "Level2" dengan nama Scene map laboratorium kamu yang sebenarnya
-        SceneManager.LoadScene("Level2"); 
+        SceneManager.LoadScene("level2"); 
     }
 }
